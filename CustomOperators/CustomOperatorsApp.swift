bef1f55bda06a340163d95fc816bdc8261d3851e
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-07-17 21:00:46 +0300

    Initial Commit

---
+++ CustomOperators/CustomOperatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomOperatorsApp.swift
+//  CustomOperators
+//
+//  Created by Ramill Ibragimov on 17.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomOperatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
