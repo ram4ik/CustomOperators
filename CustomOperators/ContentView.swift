0c515d700cf8dcf75dc1985434cb19fa546804e4
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-07-17 21:22:50 +0300

    init

--- CustomOperators/ContentView.swift
+++ CustomOperators/ContentView.swift
@@ -11,6 +11,12 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onAppear() {
+                print("\(√9)")
+                print("\(√25)")
+                print("\(42 © 69)")
+                print("\(42%)")
+            }
     }
 }
 
@@ -18,4 +24,22 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+// Prefix
+prefix operator √
+prefix func √(number: Double) -> Double {
+    return sqrt(number)
+}
+
+// Infix
+infix operator ©
+func ©(num1: Double, num2: Double) -> Double {
+    return num1 * num1 + num2 * num2
+}
+
+// Postfix
+postfix operator %
+postfix func %(number: Float) -> Float {
+    return number / 100
 }
